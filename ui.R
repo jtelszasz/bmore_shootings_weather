b765e9cfe95b15734d0f095f5daedfe57d93f1c6
Justin Elszasz <jtelszasz@gmail.com>
2018-05-01 17:31:14 -0400

    working on shiny app

--- ui.R
+++ ui.R
@@ -26,10 +26,9 @@ shinyUI(
     
     mainPanel(
       p('In 1986, the Space Shuttle Challenger exploded after launch and its crew were killed. Subsequent investigations revealed that the cause was hot gases escaping past the o-rings in the solid rocket boosters.  Cold ambient temperature (36 degrees Fahrenheit) on the morning of the launch lessened the resilience of the o-rings, resulting in "blow-by" of hot gases and an eventual explosion.  Data on the risk of thermal degradation and blow-by was available for the 23 shuttle launches prior to the Challenger, however no data existed for ambient temperatures below 53 degrees Fahrenheit.'),
-      p('This app fits a linear regression model to predict number of o-rings at risk of thermal damage based on ambient temperature in degrees Fahrenheit at launch.  The data are for 22 of the previous 23 launches (one was considered an outlier, labeled on the plot below.)  The linear model is used for ambient temperatures below 75 degrees Fahrenheit.  Above this temperature, it is assumed that there is zero thermal damage to o-rings.  If the predicted value for number of o-rings at risk is one or greater, the decision should be made not to launch.  (This occurs at ambient temperatures below 58 degrees Fahrenheit.)'),
-      h3("Ambient Temperature (deg F) at Launch:"),
       verbatimTextOutput("out.predict.week.start"),
-      verbatimTextOutput("training.start"),
+      #verbatimTextOutput("training.start"),
+      verbatimTextOutput("contents"),
       h3("Predicted Number of O-Rings at Risk for Thermal Damage"),
       p("Previous data records number of o-rings at risk (integers), but this model predicts fractional values and uses the result as a proxy to determine whether to launch."),
       #verbatimTextOutput("prediction"),
