1b541bac3a90414cb0c402a59e40948760f52689
Justin Elszasz <jtelszasz@gmail.com>
2018-05-01 21:14:28 -0400

    predictions working and shiny app published for first time

--- server.R
+++ server.R
@@ -14,8 +14,8 @@ weekly.df <- readRDS("cache.Rdata")
 apply_lags <- function(df, n.lags){
   lags <- seq(n.lags)
   lag_names <- paste("lag", 
-                     formatC(n.lags, width = nchar(max(n.lags)), flag = "0"),                      sep = "_")
-  lag_functions <- setNames(paste("dplyr::lag(., ", n.lags, ")"), lag_names)
+                     formatC(lags, width = nchar(max(lags)), flag = "0"),                      sep = "_")
+  lag_functions <- setNames(paste("dplyr::lag(., ", lags, ")"), lag_names)
  
   df <- dplyr::mutate_at(df, vars(all.shootings), funs_(lag_functions))
   
@@ -24,18 +24,16 @@ apply_lags <- function(df, n.lags){
 
 build_training_data <- function(predict.week.start){
 
-  
   training.df <- weekly.df %>%
+    apply_lags(n.lags) %>%
     dplyr::filter(week.start < as.Date(predict.week.start)) %>%
     #week.start >= (predict.week.start - lubridate::dweeks(53))) %>%
     dplyr::select(all.shootings, mean.max.temp, days.precip) %>%
-    apply_lags(n.lags) %>%
     na.omit()
   
   return(training.df)
 }
-training.data <- weekly.data %>%
-  select(-nfs, -hom, -total.precip, -uprising, -mean.min.temp)
+
 fit_weekly_model <- function(training.df){
   mod <- glm(all.shootings ~ ., 
              data = training.df, 
@@ -44,18 +42,25 @@ fit_weekly_model <- function(training.df){
 }
 
 predict_weekly_crime <- function(model, predict.week.start){
-  new <- weekly.df %>% 
-    filter(week.start == predict.week.start) %>%
+  new <- weekly.df %>%
     apply_lags(n.lags) %>%
+    filter(week.start == predict.week.start) %>%
     select(-all.shootings)
   prediction <- predict.glm(model, new, type = "response")
   return(prediction)
 }
 
+actual_n_shootings <- function(predict.week.start){
+  actual.n.shootings <- weekly.df %>%
+    filter(week.start == predict.week.start) %>%
+    select(all.shootings) %>%
+    pull()
+  return(actual.n.shootings)
+}
 
 shinyServer(function(input, output){
     
-    results <- reactive({
+    predicted.value <- reactive({
       predict.week.start <- input$predict.week.start
       
       if (is.null(predict.week.start))
@@ -75,22 +80,25 @@ shinyServer(function(input, output){
       
       #model_and_data <- build_model(new_data_df, input$sparsity)
       model <- fit_weekly_model(training.df)
-      
-      
+    
       pred <- predict_weekly_crime(model, predict.week.start)
       
       #new_data_df$Prob <- pred[,2]
       
       #new_data_df
       #model
-      pred
+      round(pred[[1]], 0)
     })
 
-    output$contents <- renderPrint({
-     results()
+    actual.value <- reactive({
+      predict.week.start <- input$predict.week.start
+      actual.n.shootings <- actual_n_shootings(predict.week.start)
+      actual.n.shootings
     })
     
-    output$out.predict.week.start <- renderPrint(input$predict.week.start)
+    output$predicted.value <- renderPrint({predicted.value()})
+    output$predict.week.start <- renderPrint(input$predict.week.start)
+    output$actual.value <- renderPrint(actual.value())
     #output$training.start <- renderPrint(pull(training.df()[1, "week.start"]))
     #output$model <- renderPrint(summary(model))
     # output$prediction <- renderPrint(predict())
@@ -103,3 +111,9 @@ shinyServer(function(input, output){
     }
   )
 
+
+
+# training.df %>%
+#   ggplot(aes(lag_01, all.shootings)) +
+#   geom_point() +
+#   geom_smooth(method = "lm")
--- ui.R
+++ ui.R
@@ -5,17 +5,18 @@ shinyUI(
   pageWithSidebar(
     
     headerPanel(
-      "Predicted and Actual Crime in Baltimore"
+      "Predicted and Actual Crime in Baltimore",
+      windowTitle = "Predicted and Actual Shootings in Baltimore"
     ),
     
     sidebarPanel(
       h2("Select the Week for Prediction"),
-      "Anticipated ambient temperature in degrees Fahrenheit at space shuttle launch time.",
 
       sliderInput("predict.week.start",
                   label="Predict for Week Starting",
-                  min = as.Date("2018-01-01"),
-                  max = floor_date(today() - dweeks(1), "week"),
+                  min = floor_date(today() - dweeks(52), "week"),
+                  max = floor_date(today() - dweeks(2), "week"), 
+                  #max = floor_date(today() - dweeks(1), "week"),
                   step = dweeks(1),
                   value = floor_date(today(), "week")
       ),
@@ -25,21 +26,22 @@ shinyUI(
     
     
     mainPanel(
-      p('In 1986, the Space Shuttle Challenger exploded after launch and its crew were killed. Subsequent investigations revealed that the cause was hot gases escaping past the o-rings in the solid rocket boosters.  Cold ambient temperature (36 degrees Fahrenheit) on the morning of the launch lessened the resilience of the o-rings, resulting in "blow-by" of hot gases and an eventual explosion.  Data on the risk of thermal degradation and blow-by was available for the 23 shuttle launches prior to the Challenger, however no data existed for ambient temperatures below 53 degrees Fahrenheit.'),
-      verbatimTextOutput("out.predict.week.start"),
+      p("The purpose of this tool is to compare a predicted number of weekly total shootings (including both non-fatal and homicides) to the actual number that occured. Data for all shootings since 2012 are used from Open Baltimore. It can be used to gauge the performance of city-wide crime interventions (lower actual shootings might be considered over-performance, while higher actual shootings might be considered under-performance of crime prevention or crime fight tactics."),
+      h3("Week of:"),
+      verbatimTextOutput("predict.week.start"),
       #verbatimTextOutput("training.start"),
-      verbatimTextOutput("contents"),
-      h3("Predicted Number of O-Rings at Risk for Thermal Damage"),
-      p("Previous data records number of o-rings at risk (integers), but this model predicts fractional values and uses the result as a proxy to determine whether to launch."),
-      #verbatimTextOutput("prediction"),
-      h3("Safe to Launch?"),
-      "For the purposes of this app, if the predicted value for number of o-rings at risk is less than one, it is safe to launch.",
-      #verbatimTextOutput("launch"),
-      h3("Data and Linear Model"),
-      p("The black line on the plot shows the linear model, with extrapolation below 53 degrees Fahrenheit (the lowest temperature at which data were available).  The blue vertical line shows the temperature at which the Challenger was launched, while the red line updates to display the temperature selected in the left side panel."),
+      
+      h3("Predicted Number of Shootings (Non-Fatal + Homicide)"),
+      verbatimTextOutput("predicted.value"),
+      
+      h3("Actual Number of Shootings (Non-Fatal + Homicide)"),
+      verbatimTextOutput("actual.value"),
+      
+      h3("Notes"),
+      p("The data used in the model to predict the number of weekly shootings include the outdoor temperature and the prior 15 weeks worth of shootings.")
+      
+  
       #plotOutput('newPlot'),
-      h3("References"),
-      "University of California - Irvine, Machine Learning Repository:", tags$a(href="https://archive.ics.uci.edu/ml/datasets/Challenger+USA+Space+Shuttle+O-Ring","Space Shuttle O-Ring Data Set")
     )
     
     
