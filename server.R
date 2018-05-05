0151600861ed60663b6a710258123d8722c7c3be
Elszasz <jtelszasz@gmail.com>
2018-05-05 16:37:11 -0400

    a couple plots

--- server.R
+++ server.R
@@ -3,10 +3,13 @@ library(tidyverse)
 library(RSocrata)
 #library(rwunderground)
 library(lubridate)
+library(broom)
 
 n.lags <- 15
 
-weekly.df <- readRDS("cache.Rdata")
+weekly.df <- readRDS("cache.Rdata") %>%
+  mutate(pred = NA_real_)
+
 
 #predict.week.start <- floor_date(today() - dweeks(1), "week")
 #train.start.week.start <- predict.week.start - dweeks(1)
@@ -28,7 +31,10 @@ build_training_data <- function(predict.week.start){
     apply_lags(n.lags) %>%
     dplyr::filter(week.start < as.Date(predict.week.start)) %>%
     #week.start >= (predict.week.start - lubridate::dweeks(53))) %>%
-    dplyr::select(all.shootings, mean.max.temp, days.precip) %>%
+    dplyr::select(all.shootings, 
+                  mean.max.temp, 
+                  days.precip, 
+                  starts_with("lag")) %>%
     na.omit()
   
   return(training.df)
@@ -57,6 +63,44 @@ actual_n_shootings <- function(predict.week.start){
     pull()
   return(actual.n.shootings)
 }
+
+
+
+# generate models/predictions for last 100 weeks
+for(i in seq(nrow(weekly.df) - 100, nrow(weekly.df), 1)){
+  predict.week.start <- weekly.df$week.start[[i]]
+  train.df <- build_training_data(predict.week.start)
+  mod <- fit_weekly_model(train.df)
+  weekly.df$pred[[i]] <- predict_weekly_crime(mod, predict.week.start)
+
+}
+
+weekly.df %>% 
+  filter(!is.na(pred)) %>%
+  ggplot(aes(week.start)) + 
+  geom_line(aes(y = all.shootings), color = "red") +
+  geom_line(aes(y = pred), color = "black")
+
+weekly.df %>% 
+  filter(!is.na(pred)) %>%
+  ggplot(aes(week.start)) + 
+  geom_col(aes(y = all.shootings), fill = "red") +
+  geom_col(aes(y = pred), fill = "black")
+
+weekly.df %>%
+  filter(!is.na(pred)) %>%
+  mutate(perf = all.shootings - pred) %>%
+  ggplot(aes(week.start, perf)) +
+  geom_col(aes(fill = (perf < 0)))
+
+weekly.df %>%
+  filter(!is.na(pred)) %>%
+  ggplot(aes(pred, all.shootings)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0) +
+  ylim(c(0,40)) +
+  xlim(c(0,40))
+
 
 shinyServer(function(input, output){
     
